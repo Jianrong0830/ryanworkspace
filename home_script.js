87316f6f5e2d020e1d6cda27a63db2c1903ab6fa
Jianrong0830 <ryanchen0830b@gmail.com>
2023-03-10 21:31:25 +0800

    a

--- home_script.js
+++ home_script.js
@@ -2,11 +2,15 @@
 function scrollToObj(obj_id){
     document.getElementById(obj_id).scrollIntoView({behavior: "smooth"});
 }
-
 // 滾動至關於我
 var intro_btn=document.getElementById('intro');
 intro_btn.addEventListener('click',function(){
     scrollToObj('page2')
+},false);
+// 滾動至頂端
+var top_btn=document.getElementById('top');
+top_btn.addEventListener('click',function(){
+    scrollToObj('page1')
 },false);
 // ---------------------------------------------------------------------------
 // Goole搜尋函數
@@ -15,12 +19,10 @@ function google_search(){
     searchUrl = 'http://www.google.com/search?q=' + query;
     window.open(searchUrl, '_blank');
 }
-
 // 搜尋觸發(按鈕)
 search_btn.addEventListener("click", function(){
     document.getElementById("search_btn").click();
 });
-
 // 搜尋觸發(Enter鍵)
 var search_bar = document.getElementById("search_bar");
 search_bar.addEventListener("keypress", function(event) {
