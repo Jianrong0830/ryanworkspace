1ffb7b05a2ab10aaefeb16a5279b84e1aaf265a5
Jianrong0830 <ryanchen0830b@gmail.com>
2023-03-25 22:48:59 +0800

    a

--- home_script.js
+++ home_script.js
@@ -3,32 +3,29 @@ function scrollToObj(obj_id){
     document.getElementById(obj_id).scrollIntoView({behavior: "smooth"});
 }
 // 滾動至關於我
-var intro_btn=document.getElementById('intro');
-intro_btn.addEventListener('click',function(){
-    scrollToObj('page2')
+navBar_intro_div.addEventListener('click',function(){
+    scrollToObj('page2_div')
 },false);
 // 滾動至頂端
-var top_btn=document.getElementById('top');
-top_btn.addEventListener('click',function(){
-    scrollToObj('page1')
+navBar_top_div.addEventListener('click',function(){
+    scrollToObj('page1_div')
 },false);
 // ---------------------------------------------------------------------------
 // Goole搜尋函數
 function google_search(){
-    query=document.getElementById('search_bar').value;
+    query=document.getElementById('googleSearch_bar').value;
     searchUrl = 'http://www.google.com/search?q=' + query;
     window.open(searchUrl, '_blank');
 }
 // 搜尋觸發(按鈕)
-search_btn.addEventListener("click", function(){
-    document.getElementById("search_btn").click();
+googleSearch_btn.addEventListener("click", function(){
+    document.getElementById("googleSearch_btn").click();
 });
 // 搜尋觸發(Enter鍵)
-var search_bar = document.getElementById("search_bar");
-search_bar.addEventListener("keypress", function(event) {
+googleSearch_bar.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
     event.preventDefault();
-    document.getElementById("search_btn").click();
+    document.getElementById("googleSearch_btn").click();
     }
 });
 // ---------------------------------------------------------------------------
