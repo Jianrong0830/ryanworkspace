745c313d5dbb31ff559ab3c0635f33cffe744f25
Ryan Chen <ryanchen0830b@gmail.com>
2023-08-11 15:25:31 +0800

    1

--- script.js
+++ script.js
@@ -2,30 +2,44 @@
 function scrollToObj(obj_id){
     document.getElementById(obj_id).scrollIntoView({behavior: "smooth"});
 }
+
 // 滾動至關於我
-navBar_aboutMe_div.addEventListener('click',function(){
-    scrollToObj('page2_div')
-},false);
+document.addEventListener('DOMContentLoaded', function() {
+    var navBar_aboutMe_div = document.getElementById('navBar_aboutMe_div');
+    navBar_aboutMe_div.addEventListener('click',function(){
+        scrollToObj('page2_div');
+    },false);
+});
+
 // 滾動至頂端
-navBar_top_div.addEventListener('click',function(){
-    scrollToObj('page1_div')
-},false);
-// ---------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+    var navBar_top_div = document.getElementById('navBar_top_div');
+    navBar_top_div.addEventListener('click',function(){
+        scrollToObj('page1_div');
+    },false);
+});
+
 // Goole搜尋函數
 function google_search(){
-    query=document.getElementById('searchBar_input').value;
+    var query=document.getElementById('searchBar_input').value;
     searchUrl = 'http://www.google.com/search?q=' + query;
     window.open(searchUrl, '_blank');
 }
+
 // 搜尋觸發(按鈕)
-searchBar_input.addEventListener("click", function(){
-    document.getElementById("searchBar_btn").click();
+document.addEventListener('DOMContentLoaded', function() {
+    var searchBar_btn=document.getElementById('searchBar_btn');
+    searchBar_btn.addEventListener("click", function(){
+        google_search();
+    });
 });
+
 // 搜尋觸發(Enter鍵)
-searchBar_input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("searchBar_btn").click();
-    }
-}); 
-// ---------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+    var searchBar_input=document.getElementById('searchBar_input');
+    searchBar_input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            google_search();
+        }
+    });
+});
